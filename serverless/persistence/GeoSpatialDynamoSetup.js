fcd0086416ff070841836a5031d6308f5d7329f8
Samuel Aquino <saquino@flexion.us>
2019-05-19 21:32:22 -0500

    Add documentation

--- serverless/persistence/GeoSpatialDynamoSetup.js
+++ serverless/persistence/GeoSpatialDynamoSetup.js
@@ -3,6 +3,7 @@ const ddbGeo = require('dynamodb-geo');
 const sampleData = require('../../sample-data/sample-data.js');
 const msabArtLocationsTableName = 'MsabArtLocationsTable';
 
+//Configures aws account information and region
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID, // export your aws access key
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, // export your aws secret key
@@ -15,7 +16,6 @@ var dynamodb = new AWS.DynamoDB();
 
 function run() {
   createTable();
-  queryRadius();
 }
 
 /**
@@ -42,7 +42,7 @@ function createTable() {
 }
 
 /**
-* Add artEntity to local database
+* Adds an artEntity to to the database called for
 */
 function addPoint(artEntity) {
   msabArtLocationsTable.putPoint({
@@ -62,18 +62,6 @@ function addPoint(artEntity) {
           }
       }).promise()
       .then(function() { console.log('Done!') });
-}
-
-function queryRadius() {
-  console.log("Querying radius...");
-  // Querying 10km from Cambridge, UK
-  msabArtLocationsTable.queryRadius({
-        RadiusInMeter: 10000,
-        CenterPoint: {
-            latitude: 51.51,
-            longitude: -0.13
-        }
-    }).then(console.log);
 }
 
 run();
---
+++ serverless/persistence/README.md
@@ -0,0 +1,18 @@
+#GeoSpatialDynamoSetup
+This is a temporary script to build a local dynamo db database.
+* The sample data comes from `sample-data.js` in the sample-data directory
+
+## Set up:
+Node is required to run the script.
+Export your aws credentials.
+example:
+* `export AWS_ACCESS_KEY_ID=YOUR_AWS_ACCESS_KEY_ID`
+* `export AWS_SECRET_ACCESS_KEY=YOUR_AWS_ACCESS_SECRET_KEY`
+
+Set up a local dynamodb instance
+See aws docs for further details: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.html
+* install the dynamodb local jar from the link above.
+* run `java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb` after installing the jar file for dynamodb from the link above. This will be a running instance of dynamodb on your machine.
+
+## To Run the script after set up:
+node serverless/persistence/GeoSpatialDynamoSetup.js
