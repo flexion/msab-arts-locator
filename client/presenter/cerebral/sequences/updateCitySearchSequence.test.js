30df1790990c61eda32cf4da6a977f897f4e4b35
Chris Barrett <cbarrett@flexion.us>
2019-05-23 16:26:52 -0500

    another tests

---
+++ client/presenter/cerebral/sequences/updateCitySearchSequence.test.js
@@ -0,0 +1,19 @@
+import { CerebralTest } from 'cerebral/test';
+import { createMockApplicationContext } from '../../../utilities/TestUtils';
+import { presenter } from '../../../presenter/presenter';
+
+describe('updateCitySearchSequence', () => {
+  it('should update cerebral state with city', async () => {
+    const mockApplicationContext = createMockApplicationContext();
+
+    presenter.providers.applicationContext = mockApplicationContext;
+    const test = CerebralTest(presenter);
+    test.setState('cityValue', '');
+
+    await test.runSequence('updateCitySearchSequence', {
+      cityValue: 'Mankato',
+    });
+
+    expect(test.getState('cityValue')).toMatch('Mankato');
+  });
+});
