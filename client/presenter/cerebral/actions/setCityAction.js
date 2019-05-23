c0590acb398efa5b89d2d09abae6d51e7bf7ac5a
Chris Barrett <cbarrett@flexion.us>
2019-05-23 16:24:39 -0500

    another test

--- client/presenter/cerebral/actions/setCityAction.js
+++ client/presenter/cerebral/actions/setCityAction.js
@@ -1,5 +1,5 @@
 import { state } from 'cerebral';
 
 export const setCityAction = ({ store, props }) => {
-  store.set(state.cityValue, props.result.data.cityValue);
+  store.set(state.cityValue, props.cityValue);
 };
