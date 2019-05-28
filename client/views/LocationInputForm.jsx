7883f19ee2ee48728ad40957eba3e1a9e733f98b
Chris Barrett <cbarrett@flexion.us>
2019-05-28 17:20:24 -0500

    adding fields to form

--- client/views/LocationInputForm.jsx
+++ client/views/LocationInputForm.jsx
@@ -20,6 +20,19 @@ export const LocationInputForm = connect(
     updateFormValueSequence: sequences.updateFormValueSequence,
   },
   ({ form, submitLocation, updateFormValueSequence }) => {
+    /*
+
+Location Name
+Location Phone Number - Optional?
+Category/Discipline - multiple select
+Address - Street address, City, State, Zip
+Photo
+URL - Optional?
+Contact person
+Contact Email
+Contact phone
+? Description ?*/
+
     return (
       <Section className="msab-has-background-grey">
         <Container>
@@ -37,7 +50,7 @@ export const LocationInputForm = connect(
             }}
           >
             <Field>
-              <Label>Artist Name</Label>
+              <Label className="has-text-white">Artist Name</Label>
               <Control>
                 <Input
                   type="text"
@@ -53,7 +66,7 @@ export const LocationInputForm = connect(
               </Control>
             </Field>
             <Field>
-              <Label>Location Name</Label>
+              <Label className="has-text-white">Location Name</Label>
               <Control>
                 <Input
                   type="text"
@@ -69,12 +82,12 @@ export const LocationInputForm = connect(
               </Control>
             </Field>
             <Field>
-              <Label>Phone Number</Label>
+              <Label className="has-text-white">Website URL</Label>
               <Control>
                 <Input
-                  isColor="success"
-                  name="phoneNumber"
-                  value={form.phoneNumber || ''}
+                  type="text"
+                  name="website"
+                  value={form.website || ''}
                   onChange={(e) => {
                     updateFormValueSequence({
                       key: e.target.name,
@@ -84,9 +97,73 @@ export const LocationInputForm = connect(
                 />
               </Control>
             </Field>
-
             <Field>
-              <Label>Categories:</Label>
+              <Label className="has-text-white">Street Address</Label>
+              <Control>
+                <Input
+                  type="text"
+                  name="street"
+                  value={form.street || ''}
+                  onChange={(e) => {
+                    updateFormValueSequence({
+                      key: e.target.name,
+                      value: e.target.value,
+                    });
+                  }}
+                />
+              </Control>
+            </Field>
+            <Field>
+              <Label className="has-text-white">City</Label>
+              <Control>
+                <Input
+                  type="text"
+                  name="city"
+                  value={form.city || ''}
+                  onChange={(e) => {
+                    updateFormValueSequence({
+                      key: e.target.name,
+                      value: e.target.value,
+                    });
+                  }}
+                />
+              </Control>
+            </Field>
+            <Field>
+              <Label className="has-text-white">State</Label>
+              <Control>
+                <Input
+                  isColor="success"
+                  name="state"
+                  value={form.state || 'MN'}
+                  disabled
+                  onChange={(e) => {
+                    updateFormValueSequence({
+                      key: e.target.name,
+                      value: e.target.value,
+                    });
+                  }}
+                />
+              </Control>
+            </Field>
+            <Field>
+              <Label className="has-text-white">Zip</Label>
+              <Control>
+                <Input
+                  isColor="success"
+                  name="zip"
+                  value={form.zip || 'MN'}
+                  onChange={(e) => {
+                    updateFormValueSequence({
+                      key: e.target.name,
+                      value: e.target.value,
+                    });
+                  }}
+                />
+              </Control>
+            </Field>
+            <Field>
+              <Label className="has-text-white">Categories:</Label>
               <Control>
                 <Select
                   name="category"
@@ -107,6 +184,54 @@ export const LocationInputForm = connect(
                   <option>Folk/Tradition</option>
                   <option>Literary</option>
                 </Select>
+              </Control>
+            </Field>
+            <Field>
+              <Label className="has-text-white">Contact Name</Label>
+              <Control>
+                <Input
+                  type="text"
+                  name="contactName"
+                  value={form.contactName || ''}
+                  onChange={(e) => {
+                    updateFormValueSequence({
+                      key: e.target.name,
+                      value: e.target.value,
+                    });
+                  }}
+                />
+              </Control>
+            </Field>
+            <Field>
+              <Label className="has-text-white">Contact Phone</Label>
+              <Control>
+                <Input
+                  type="text"
+                  name="contactPhone"
+                  value={form.contactPhone || ''}
+                  onChange={(e) => {
+                    updateFormValueSequence({
+                      key: e.target.name,
+                      value: e.target.value,
+                    });
+                  }}
+                />
+              </Control>
+            </Field>
+            <Field>
+              <Label className="has-text-white">Contact Email</Label>
+              <Control>
+                <Input
+                  type="text"
+                  name="contactEmail"
+                  value={form.contactEmail || ''}
+                  onChange={(e) => {
+                    updateFormValueSequence({
+                      key: e.target.name,
+                      value: e.target.value,
+                    });
+                  }}
+                />
               </Control>
             </Field>
             <Field isGrouped>
