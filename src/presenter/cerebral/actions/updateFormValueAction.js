f22987dd1b49af2a69a38cda4f6f5b5688667ed4
Chris Barrett <cbarrett@flexion.us>
2019-06-21 18:35:02 -0500

    remove console

--- src/presenter/cerebral/actions/updateFormValueAction.js
+++ src/presenter/cerebral/actions/updateFormValueAction.js
@@ -4,6 +4,5 @@ export const updateFormValueAction = ({ store, props }) => {
   if (props.value === '') {
     props.value = null;
   }
-  console.log('yuno', props);
   store.set(state.form[props.key], props.value);
 };
