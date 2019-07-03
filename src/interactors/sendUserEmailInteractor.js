ab8f8bacf7b5f0a75f4d21c494a503ac6523d5b9
Natalie Kurz <nkurz@flexion.us>
2019-07-03 23:58:32 +0200

    Fixed button text

--- src/interactors/sendUserEmailInteractor.js
+++ src/interactors/sendUserEmailInteractor.js
@@ -39,7 +39,7 @@ exports.sendUserEmail = async ({
         <p>3. Press the “Update” button.</p>
         <p>4. The Arts Board will review your changes.</p>
         <p>5. You’ll get an e-mail letting you know if your changes were approved or not.</p>
-        <p>If you have questions or would like us to remove your listing, please contact us at <a href = "mailto: artsaroundmn.admin@state.mn.us">artsaroundmn.admin@state.mn.us</a>.</p>
+        <p>If you have questions or would like us to remove your listing, please contact us at <a href ="mailto: artsaroundmn.admin@state.mn.us">artsaroundmn.admin@state.mn.us</a>.</p>
         <p>Thank you again for being a part of Arts Around MN!</p>
         <p>Sincerely,</p>
         <p>The Minnesota State Arts Board</p>
@@ -50,7 +50,7 @@ exports.sendUserEmail = async ({
         to: artLocation.contactEmail,
         subject: `Your submission to Arts Around MN was not approved`,
         body: `<p>Thank you for your interest in being listed on Arts Around MN.</p>
-        <p>The Minnesota State Arts Board reserves the right to approve or deny any content submitted.  Based on our review of the information you submitted, we have determined that we won’t be able to add your listing to the site. </p>
+        <p>The Minnesota State Arts Board reserves the right to approve or deny any content submitted. Based on our review of the information you submitted, we have determined that we won’t be able to add your listing to the site. </p>
         <p>Sincerely,</p>
         <p>The Minnesota State Arts Board</p>
         `,
