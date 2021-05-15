748ed7da8b97cc52c5d1af623144ad55be3276be
Brice Ruth <bruth@flexion.us>
2021-05-14 22:52:36 -0500

    Finish/test `loadData.js` script.
    * Run as: AWS_REGION=us-east-2 GIS_TABLE=msab-arts-locator-gis-dev API_KEY=<geocoder-api-key> node environments/local/loadData.js with AWS creds in environment or default profile.

--- src/environments/local/ApplicationContext.js
+++ src/environments/local/ApplicationContext.js
@@ -1,10 +1,45 @@
 const { v4: uuidv4 } = require('uuid');
 const { validateJson } = require('../../utilities/AjvJsonValidator');
+const {
+ getLocationCoordinates,
+} = require('../../interactors/getLocationCoordinatesInteractor');
+const environment = {
+ apiKey: process.env.API_KEY,
+};
+const { getCoordsFromAddress } = require('../../persistence/MapsAPIGateway');
+const apiURLs = {
+ geocodeAPIUrl: `https://maps.googleapis.com/maps/api/geocode/json`,
+};
+const {
+ saveNewArtLocation,
+} = require('../../interactors/saveNewArtLocationInteractor');
+const {
+ saveNewLocationGeo,
+} = require('../../persistence/GeoDynamoGateway');
 
 module.exports = () => {
   return {
+    apiURLs: () => {
+     return apiURLs;
+    },
+    getUseCases: () => {
+     return {
+      getLocationCoordinates,
+      saveNewArtLocation,
+     }
+    },
+    environment,
+    getPersistenceGateway: () => {
+     return {
+      getCoordsFromAddress,
+      saveNewLocationGeo,
+     }
+    },   
     getUniqueIdString: () => {
       return uuidv4();
+    },
+    getUniqueId: () => {
+     return uuidv4();
     },
     getCurrentTimestamp: () => {
       return Date.now();
--- src/environments/local/loadData.js
+++ src/environments/local/loadData.js
@@ -1,34 +1,30 @@
 const localApplicationContext = require('./ApplicationContext');
 const { createNewLocation } = require('../../controllers/createNewLocation');
+const sampleData = require('../../../sample-data/sample-data');
 
-const main = async ({ data }) => {
-  const result = await createNewLocation({
-    locationData: data,
-    applicationContext: localApplicationContext(),
-  });
-  console.log(result);
-};
+const main = async (entities) => {
+  return Promise.all(entities.map(transformSampleData).map(entity => createNewLocation({
+   locationData: entity,
+   applicationContext: localApplicationContext(),
+ })));
+}
 
-let data = {
- name: 'Another Cool Art Location',
- category: {
-   craft: true,
-   dance: true,
-   folk: true,
-   literary: true,
-   music: true,
-   photo: true,
-   opera: true,
-   visual: true
- },
- street: '730 31st St NE',
- city: 'Rochester',
- state: 'MN',
- zip: '55906',
- contactName: 'Brice',
- contactEmail: 'bruth@flexion.us',
- contactPhone: '555-123-2345',
- gresp: '03AGdBq2421EsxsLwlQqp6hVIkRb7o-01a7eJmbD1mqkreiwJ21_VvKOS2EXybQmRYLX4hfOtHh-ovZjDEXOfzUL04D_59f-FGVXnCyxp3xzwzWPkZgbHhvkvVf-U8T2GbYohHUDazg4Lb7-q9FLH8iAI5UUTKXYLSxccn7Wq084qkVOERE1w5SREDK45mArq30WUnKW7wbVMnS-fpUQG4eo8fzKPm0h36HjAKc4NbriMltR58Cw42Zn5iWg-DfTxuNOnQTOwNz0lo0zTNEto6XlGwJu8xjjn-yiX1MQJdv-CKLrCdXdEBiY4mq-RHOnL4VEeIaAA4lMOL6KEx8y45F4bKFETYMHeSSCHOhyB9s666VQusJPlL3Muj8f2JxfYdC_KnGSadv0LTpBdV1I1wfM4BeilIqs8L1HB_Jy9LXt4VYJY7YbDIEoJZbprox7awY78uLK-89FOn',
- approved: true,
-};
-main({ data });
+const transformSampleData = (data) => {
+ return {
+  name: data.name,
+  street: data.address1,
+  city: data.city,
+  state: data.st,
+  zip: data.zip,
+  contactName: data.primary_contact,
+  contactEmail: data.email,
+  contactPhone: data.phone,
+  gresp: 'abc123',
+  approved: true,
+  category: {
+   visual: true,
+  }
+ }
+}
+
+main(sampleData);
