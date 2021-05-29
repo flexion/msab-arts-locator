8b716be7fda2ea1215fc0368a0c5670102d03c14
Brice Ruth <bruth@flexion.us>
2021-05-28 23:15:53 -0500

    Create `saveLocation` controller & test.

---
+++ src/controllers/saveLocation.js
@@ -0,0 +1,166 @@
+const save = async ({ applicationContext, requestData }) => {
+  const { checkUpdate } = applicationContext.getUseCases();
+  let saveResult = null;
+  let validateResult = true;
+  let msg = null;
+  let imageUrl = null;
+  let artLocation = null;
+
+  const captchaResult = await applicationContext
+    .getUseCases()
+    .validateCaptcha({ applicationContext, value: requestData.gresp });
+  console.log('captcharesult: ', captchaResult);
+  requestData.approved = false; // always false, if admin action handled later
+  if (captchaResult.status === 'success') {
+    const {
+      isUpdate,
+      isUpdateValid,
+      locationData: currentLocationData,
+    } = await checkUpdate({
+      applicationContext,
+      locationIds: requestData.update,
+    });
+    if (isUpdateValid) {
+      validateResult = await applicationContext
+        .getUseCases()
+        .validateArtLocation({
+          applicationContext,
+          requestData,
+        });
+      msg = validateResult.status;
+      if (msg === 'success') {
+        ({ artLocation } = validateResult);
+        const coordResult = await applicationContext
+          .getUseCases()
+          .getLocationCoordinates({
+            applicationContext,
+            artLocation,
+          });
+        msg = coordResult.status;
+
+        console.log('coordResult: ', coordResult);
+        if (msg === 'success') {
+          if (requestData.base64Image) {
+            validateResult = await applicationContext
+              .getUseCases()
+              .validateImageFileType(requestData.base64Image);
+            msg = validateResult.status;
+            if (msg === 'success') {
+              saveResult = await applicationContext
+                .getUseCases()
+                .putArtLocationImage(
+                  {
+                    base64Image: requestData.base64Image,
+                    contentType: validateResult.contentType.type,
+                    entityId: artLocation.entityId,
+                    ext: validateResult.contentType.ext,
+                  },
+                  applicationContext,
+                );
+              msg = saveResult.status;
+              imageUrl = saveResult.data.Location;
+            }
+          }
+
+          if (msg === 'success') {
+            if (imageUrl) {
+              artLocation.imageURL = imageUrl;
+            } else if (currentLocationData && currentLocationData.imageURL) {
+              artLocation.imageURL = currentLocationData.imageURL;
+            }
+            if (isUpdate) {
+              //need replace admin and update id with existing ids
+              artLocation.adminId = currentLocationData.adminId;
+              artLocation.updateId = currentLocationData.updateId;
+              if (requestData.update.actionType === 'admin') {
+                artLocation.approved = true;
+              } else {
+                // admins need to re-approve changes
+                artLocation.approved = false;
+              }
+            }
+
+            artLocation.city = coordResult.cityName;
+            console.log(
+              'setting city name based on geocoords: ',
+              coordResult.cityName,
+            );
+            saveResult = await applicationContext
+              .getUseCases()
+              .saveNewArtLocation({
+                applicationContext,
+                artLocation,
+                coords: coordResult.coords,
+              });
+            msg = saveResult.status;
+            console.log('saveResult: ', saveResult);
+          }
+        }
+      }
+      if (msg === 'success' && isUpdate) {
+        //need to delete previous location record
+        const deleteResults = await applicationContext
+          .getUseCases()
+          .deleteArtLocation({
+            applicationContext,
+            requestData: currentLocationData,
+          });
+        console.log('delete results: ', deleteResults);
+        if (deleteResults.status !== 'success') {
+          //uh oh, we might now have duplicate rows
+          //email admin?
+        }
+      }
+      if (msg === 'success') {
+        let emailResults = null;
+        if (isUpdate) {
+          if (artLocation.approved) {
+            //is admin action
+            emailResults = await applicationContext
+              .getUseCases()
+              .sendUserEmail({
+                applicationContext,
+                approved: true,
+                artLocation,
+                initial: false,
+              });
+            console.log('user email Results: ', emailResults);
+          } else {
+            // user updated action
+            emailResults = await applicationContext
+              .getUseCases()
+              .sendAdminEmail({
+                applicationContext,
+                artLocation,
+              });
+            console.log('admin email Results: ', emailResults);
+          }
+        } else {
+          //is first time submission or update
+          emailResults = await applicationContext.getUseCases().sendAdminEmail({
+            applicationContext,
+            artLocation,
+          });
+          console.log('admin email Results: ', emailResults);
+          emailResults = await applicationContext.getUseCases().sendUserEmail({
+            applicationContext,
+            approved: false,
+            artLocation,
+            initial: true,
+          });
+          console.log('user email Results: ', emailResults);
+        }
+
+        return 'success';
+      } else {
+        return msg;
+      }
+    }
+  } else {
+    throw new Error(
+      `error: ${captchaResult.msg}, status: ${captchaResult.status}`,
+    );
+  }
+};
+
+module.exports = { save };
---
+++ src/controllers/saveLocation.test.js
@@ -0,0 +1,331 @@
+const { createMockApplicationContext } = require('../utilities/TestUtils');
+const { save: saveLocation } = require('./saveLocation');
+
+let captchaSuccess = 'failure';
+let usecasesCalled = [];
+const mockApplicationContext = createMockApplicationContext({
+  getUseCases: () => {
+    return {
+      checkUpdate: async () => {
+        usecasesCalled.push('checkUpdate');
+        return {
+          isUpdateValid: true,
+        };
+      },
+      deleteArtLocation: async () => {
+        usecasesCalled.push('deleteArtLocation');
+        return {
+          status: 'success',
+        };
+      },
+      getLocationCoordinates: async () => {
+        usecasesCalled.push('getLocationCoordinates');
+        return {
+          cityName: 'Mankato',
+          status: 'success',
+        };
+      },
+      putArtLocationImage: async () => {
+        usecasesCalled.push('putArtLocationImage');
+        return {
+          data: {
+            Location: 'http://example.com',
+          },
+          status: 'success',
+        };
+      },
+      saveNewArtLocation: async () => {
+        usecasesCalled.push('saveNewArtLocation');
+        return {
+          status: 'success',
+        };
+      },
+      sendAdminEmail: async () => {
+        usecasesCalled.push('sendAdminEmail');
+      },
+      sendUserEmail: async () => {
+        usecasesCalled.push('sendUserEmail');
+      },
+      validateArtLocation: async () => {
+        usecasesCalled.push('validateArtLocation');
+        return {
+          artLocation: {},
+          status: 'success',
+        };
+      },
+      validateCaptcha: async () => {
+        usecasesCalled.push('validateCaptcha');
+        return {
+          status: captchaSuccess,
+        };
+      },
+      validateImageFileType: async () => {
+        usecasesCalled.push('validateImageFileType');
+        return {
+          contentType: {
+            type: 'image/jpg',
+          },
+          status: 'success',
+        };
+      },
+    };
+  },
+});
+
+describe('saveLocation controller', () => {
+  it('should throw an error if the captcha is not valid', async () => {
+    captchaSuccess = 'failure';
+    usecasesCalled = [];
+    await expect(
+      saveLocation({
+        applicationContext: mockApplicationContext,
+        requestData: {},
+      }),
+    ).rejects.toThrowError(/failure/);
+    expect(usecasesCalled).toEqual(['validateCaptcha']);
+  });
+
+  it('should validate art location', async () => {
+    captchaSuccess = 'success';
+    const mockAppContext = {
+      ...mockApplicationContext,
+      getUseCases: () => {
+        return {
+          ...mockApplicationContext.getUseCases(),
+          validateArtLocation: async () => {
+            usecasesCalled.push('validateArtLocation');
+            return {
+              status: 'failure',
+            };
+          },
+        };
+      },
+    };
+
+    usecasesCalled = [];
+    await saveLocation({
+      applicationContext: mockAppContext,
+      requestData: {},
+    });
+    expect(usecasesCalled).toEqual([
+      'validateCaptcha',
+      'checkUpdate',
+      'validateArtLocation',
+    ]);
+  });
+
+  it('should save a new location without an image', async () => {
+    captchaSuccess = 'success';
+
+    usecasesCalled = [];
+    await saveLocation({
+      applicationContext: mockApplicationContext,
+      requestData: {},
+    });
+    expect(usecasesCalled).toEqual([
+      'validateCaptcha',
+      'checkUpdate',
+      'validateArtLocation',
+      'getLocationCoordinates',
+      'saveNewArtLocation',
+      'sendAdminEmail',
+      'sendUserEmail',
+    ]);
+  });
+
+  it('should save a new location with an image', async () => {
+    captchaSuccess = 'success';
+
+    usecasesCalled = [];
+    await saveLocation({
+      applicationContext: mockApplicationContext,
+      requestData: { base64Image: 1 },
+    });
+    expect(usecasesCalled).toEqual([
+      'validateCaptcha',
+      'checkUpdate',
+      'validateArtLocation',
+      'getLocationCoordinates',
+      'validateImageFileType',
+      'putArtLocationImage',
+      'saveNewArtLocation',
+      'sendAdminEmail',
+      'sendUserEmail',
+    ]);
+  });
+
+  it('should save an update without any images', async () => {
+    captchaSuccess = 'success';
+    const modifiedMockApplicationContext = {
+      ...mockApplicationContext,
+      getUseCases: () => {
+        return {
+          ...mockApplicationContext.getUseCases(),
+          checkUpdate: async () => {
+            usecasesCalled.push('checkUpdate');
+            return {
+              isUpdate: true,
+              isUpdateValid: true,
+              locationData: {},
+            };
+          },
+        };
+      },
+    };
+
+    usecasesCalled = [];
+    await saveLocation({
+      applicationContext: modifiedMockApplicationContext,
+      requestData: { update: {} },
+    });
+    expect(usecasesCalled).toEqual([
+      'validateCaptcha',
+      'checkUpdate',
+      'validateArtLocation',
+      'getLocationCoordinates',
+      'saveNewArtLocation',
+      'deleteArtLocation',
+      'sendAdminEmail',
+    ]);
+  });
+
+  it('should save an update with a new image', async () => {
+    captchaSuccess = 'success';
+    const modifiedMockApplicationContext = {
+      ...mockApplicationContext,
+      getUseCases: () => {
+        return {
+          ...mockApplicationContext.getUseCases(),
+          checkUpdate: async () => {
+            usecasesCalled.push('checkUpdate');
+            return {
+              isUpdate: true,
+              isUpdateValid: true,
+              locationData: {},
+            };
+          },
+          putArtLocationImage: async () => {
+            usecasesCalled.push('putArtLocationImage');
+            return {
+              data: {
+                Location: 'http://example.com',
+              },
+              status: 'success',
+            };
+          },
+          saveNewArtLocation: async ({ artLocation }) => {
+            usecasesCalled.push('saveNewArtLocation');
+            expect(artLocation.imageURL).toEqual('http://example.com');
+            return {
+              status: 'success',
+            };
+          },
+        };
+      },
+    };
+
+    usecasesCalled = [];
+    expect.assertions(2);
+    await saveLocation({
+      applicationContext: modifiedMockApplicationContext,
+      requestData: { base64Image: 1, update: {} },
+    });
+    expect(usecasesCalled).toEqual([
+      'validateCaptcha',
+      'checkUpdate',
+      'validateArtLocation',
+      'getLocationCoordinates',
+      'validateImageFileType',
+      'putArtLocationImage',
+      'saveNewArtLocation',
+      'deleteArtLocation',
+      'sendAdminEmail',
+    ]);
+  });
+
+  it('should save an update for ArtLocation with existing image', async () => {
+    captchaSuccess = 'success';
+    const modifiedMockApplicationContext = {
+      ...mockApplicationContext,
+      getUseCases: () => {
+        return {
+          ...mockApplicationContext.getUseCases(),
+          checkUpdate: async () => {
+            usecasesCalled.push('checkUpdate');
+            return {
+              isUpdate: true,
+              isUpdateValid: true,
+              locationData: { imageURL: 'foo' },
+            };
+          },
+          saveNewArtLocation: async ({ artLocation }) => {
+            usecasesCalled.push('saveNewArtLocation');
+            expect(artLocation.imageURL).toEqual('foo');
+            return {
+              status: 'success',
+            };
+          },
+        };
+      },
+    };
+
+    usecasesCalled = [];
+    expect.assertions(2);
+    await saveLocation({
+      applicationContext: modifiedMockApplicationContext,
+      requestData: { update: {} },
+    });
+    expect(usecasesCalled).toEqual([
+      'validateCaptcha',
+      'checkUpdate',
+      'validateArtLocation',
+      'getLocationCoordinates',
+      'saveNewArtLocation',
+      'deleteArtLocation',
+      'sendAdminEmail',
+    ]);
+  });
+
+  it('should save an admin update with approval', async () => {
+    captchaSuccess = 'success';
+    const modifiedMockApplicationContext = {
+      ...mockApplicationContext,
+      getUseCases: () => {
+        return {
+          ...mockApplicationContext.getUseCases(),
+          checkUpdate: async () => {
+            usecasesCalled.push('checkUpdate');
+            return {
+              isUpdate: true,
+              isUpdateValid: true,
+              locationData: {},
+            };
+          },
+          saveNewArtLocation: async ({ artLocation }) => {
+            usecasesCalled.push('saveNewArtLocation');
+            expect(artLocation.approved).toEqual(true);
+            return {
+              status: 'success',
+            };
+          },
+        };
+      },
+    };
+
+    usecasesCalled = [];
+    expect.assertions(2);
+    await saveLocation({
+      applicationContext: modifiedMockApplicationContext,
+      requestData: { update: { actionType: 'admin' } },
+    });
+    expect(usecasesCalled).toEqual([
+      'validateCaptcha',
+      'checkUpdate',
+      'validateArtLocation',
+      'getLocationCoordinates',
+      'saveNewArtLocation',
+      'deleteArtLocation',
+      'sendUserEmail',
+    ]);
+  });
+});
